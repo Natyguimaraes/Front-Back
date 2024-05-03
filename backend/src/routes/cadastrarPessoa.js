cc0ffab77c6d51725851e964cfd057f6f9f947ec
Natyguimaraes <santosnath433@gmail.com>
2024-05-03 16:30:03 -0300

    atualização

---
+++ backend/src/routes/cadastrarPessoa.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+import CadastroPessoa from "../../../src/CadastroPessoa.js";
+
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/cadastrarPessoa', (req, res) => {
+    const {nome, idade, cpf } = req.body;
+
+    const novaPessoa = new CadastroPessoa(nome, idade, cpf);
+
+    res.status(201).json({ mensagem: 'Pessoa cadastrada com sucesso', pessoa: novaPessoa });
+});
+
+app.listen(3000, () => {
+    console.log(`Servidor Express rodando como sucesso porta 3000`);
+});
+
